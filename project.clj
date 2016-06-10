306658f0697c28252efe23559baa46ec9d2608b6
James Sofra <james.sofra@gmail.com>
2016-06-10 14:06:49 +1000

    Update Spark version.

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [gorillalabs/sparkling "1.2.3"]
+                 [gorillalabs/sparkling "1.2.5"]
                  [clj-http "2.1.0"]
                  [enlive "1.1.6"]]
 
   :aot [#".*" sparkling.serialization sparkling.destructuring]
 
-  :profiles {:provided {:dependencies [[org.apache.spark/spark-core_2.10 "1.5.1"]]}
+  :profiles {:provided {:dependencies [[org.apache.spark/spark-core_2.11 "1.6.1"]]}
              :dev {:plugins [[lein-dotenv "RELEASE"]]}})
