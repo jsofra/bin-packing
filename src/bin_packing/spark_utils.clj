dc7e10659f8ece8f7e09a73ba202c322e4ffc28f
James Sofra <james.sofra@gmail.com>
2016-06-16 09:40:01 +1000

    Updates and corrections of some functions in spark_utils

--- src/bin_packing/spark_utils.clj
+++ src/bin_packing/spark_utils.clj
@@ -3,7 +3,8 @@
             [sparkling.destructuring :as de])
   (:import org.apache.spark.api.java.JavaRDD
            org.apache.spark.api.java.JavaPairRDD)
-  (:refer-clojure :exclude [map count group-by mapcat reduce filter select-keys]))
+  (:refer-clojure :exclude [map count group-by mapcat
+                            reduce filter select-keys get]))
 
 (defprotocol Transformers
   (xmap [coll f])
@@ -15,6 +16,7 @@
   (xcount [coll])
   (xselect-keys [coll ks])
   (xto-map [coll])
+  (xget [coll k d])
   (xcache [coll]))
 
 (defn -map-vals [f coll]
@@ -39,12 +41,13 @@
   (xmap [coll f] (spark/map (de/key-value-fn (fn [k v] (f [k v]))) coll))
   (xmap-vals [coll f] (spark/map-values f coll))
   (xgroup-by [coll f] (spark/group-by f coll))
-  (xmapcat [coll f] (spark/flat-map f coll))
+  (xmapcat [coll f] (spark/flat-map (de/key-value-fn (fn [k v] (f [k v]))) coll))
   (xreduce [coll f] (spark/reduce f coll))
   (xfilter [coll f] (spark/filter (de/key-value-fn (fn [k v] (f [k v]))) coll))
   (xselect-keys [coll ks] (spark/filter (de/key-fn (into #{} ks)) coll))
-  (xto-map [coll] (xto-map (xmap coll (partial apply vector))))
+  (xto-map [coll] (spark/collect-map coll))
   (xcount [coll] (spark/count coll))
+  (xget [coll k d] (or (first (spark/lookup coll k)) d))
   (xcache [coll] (spark/cache coll))
   clojure.lang.IPersistentMap
   (xmap [coll f] (clojure.core/map f coll))
@@ -52,6 +55,7 @@
   (xselect-keys [coll ks] (clojure.core/select-keys coll ks))
   (xto-map [coll] (into {} coll))
   (xcount [coll] (clojure.core/count coll))
+  (xget [coll k d] (clojure.core/get coll k))
   (xcache [coll] coll)
   clojure.lang.IPersistentCollection
   (xmap [coll f] (clojure.core/map f coll))
@@ -63,6 +67,7 @@
   (xselect-keys [coll ks] (clojure.core/select-keys coll ks))
   (xto-map [coll] (into {} coll))
   (xcount [coll] (clojure.core/count coll))
+  (xget [coll k d] (or (first (clojure.core/filter #(= (first %) k) coll)) d))
   (xcache [coll] coll))
 
 (defn map [f coll] (xmap coll f))
@@ -74,4 +79,5 @@
 (defn select-keys [coll ks] (xselect-keys coll ks))
 (defn to-map [coll] (xto-map coll))
 (defn count [coll] (xcount coll))
+(defn get [coll k d] (xget coll k d))
 (defn cache [coll] (xcache coll))
